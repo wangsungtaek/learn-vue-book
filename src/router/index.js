97ce2a6a6e9e3965efaac96fa8dec54e0f46a550
wangsungtaek <tjdxorv@naver.com>
2022-03-24 23:29:07 +0900

    서버 데이터 바인딩 실습

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/data',
+    name: 'Data',
+    component: () => import(/* webpackChunkName: "data" */ '../views/DataBindingList2.vue')
   }
+
 ]
 
 const router = createRouter({
